390db610ba677f50bc52d6131def5e1bbb579738
TakedaONe <anuaraidar15@gmail.com>
2024-07-05 11:47:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/les31/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.les31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText subjectEditText;
+    private EditText bodyEditText;
+    private Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        subjectEditText = findViewById(R.id.subjectEditText);
+        bodyEditText = findViewById(R.id.bodyEditText);
+        sendButton = findViewById(R.id.sendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+    }
+
+    private void sendEmail() {
+        String email = emailEditText.getText().toString();
+        String subject = subjectEditText.getText().toString();
+        String body = bodyEditText.getText().toString();
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send email using..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+        }
+    }
+}
